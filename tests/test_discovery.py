6584dcfdd4a81f6499f2c63019391f2d25da84fa
lambtron <andyjiang@gmail.com>
2018-06-21 19:43:32 -0700

    add test for merge table

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -13,8 +13,53 @@ from utils import get_test_connection, ensure_test_table
 LOGGER = get_logger()
 
 
-class TestTable(unittest.TestCase):
-    table_name = 'TEST_TABLE'
+class TestEventsTable(unittest.TestCase):
+    table_name = "TEST_EVENTS_TABLE"
+    schema_name = "PUBLIC"
+    key_property = "UUID"
+
+
+    def setup(self):
+        table_spec = { "columns": [
+                       { "name": "string", "type": "STRING" },
+                       { "name": "integer", "type": "INTEGER" },
+                       { "name": "time_created", "type": "TIMESTAMP" },
+                       { "name": "object", "type": "VARIANT" },
+                       { "name": "boolean", "type": "BOOLEAN" },
+                       { "name": "number", "type": "NUMBER" },
+                       { "name": "date_created", "type": "DATE" }
+                     ],
+                       "schema": TestEventsTable.schema_name,
+                       "name": TestEventsTable.table_name }
+        con = get_test_connection()
+        ensure_test_table(con, table_spec)
+
+
+    def test_catalog(self):
+        con = get_test_connection()
+        catalog = tap_amplitude.discover_catalog(con).to_dict()
+
+        test_streams = [s for s in catalog['streams'] if s['tap_stream_id'] == "{}-{}".format(TestEventsTable.schema_name, TestEventsTable.table_name)]
+
+        # Is there one stream found with same name?
+        self.assertEqual(len(test_streams), 1)
+
+        stream_dict = test_streams[0]
+        self.assertEqual(TestEventsTable.table_name, stream_dict.get('table_name'))
+        self.assertEqual(TestEventsTable.table_name, stream_dict.get('stream'))
+
+        # Check primary key is "UUID".
+        mdata = metadata.to_map(stream_dict['metadata'])
+        stream_metadata = mdata.get((), {})
+        key_properties = stream_metadata.get('table-key-properties', [])
+        self.assertEqual(TestEventsTable.key_property, key_properties[0])
+
+        # Check metadata.
+
+
+
+class TestMergeTable(unittest.TestCase):
+    table_name = 'TEST_MERGE_TABLE'
     schema_name = 'PUBLIC'
 
 
@@ -28,8 +73,8 @@ class TestTable(unittest.TestCase):
                        { "name": "number", "type": "NUMBER" },
                        { "name": "date_created", "type": "DATE" }
                      ],
-                       "schema": TestTable.schema_name,
-                       "name": TestTable.table_name }
+                       "schema": TestMergeTable.schema_name,
+                       "name": TestMergeTable.table_name }
         con = get_test_connection()
         ensure_test_table(con, table_spec)
 
@@ -38,24 +83,33 @@ class TestTable(unittest.TestCase):
         con = get_test_connection()
         catalog = tap_amplitude.discover_catalog(con).to_dict()
 
-        test_streams = [s for s in catalog['streams'] if s['tap_stream_id'] == "{}-{}".format(TestTable.schema_name, TestTable.table_name)]
+        test_streams = [s for s in catalog['streams'] if s['tap_stream_id'] == "{}-{}".format(TestMergeTable.schema_name, TestMergeTable.table_name)]
 
         # Is there one stream found with same name?
         self.assertEqual(len(test_streams), 1)
 
+        # Check table_stream and stream name.
         stream_dict = test_streams[0]
-        self.assertEqual(TestTable.table_name, stream_dict.get('table_name'))
-        self.assertEqual(TestTable.table_name, stream_dict.get('stream'))
+        self.assertEqual(TestMergeTable.table_name, stream_dict.get('table_name'))
+        self.assertEqual(TestMergeTable.table_name, stream_dict.get('stream'))
 
+        # Check that there is no key property.
+        mdata = metadata.to_map(stream_dict['metadata'])
+        stream_metadata = mdata.get((), {})
+        key_properties = stream_metadata.get('table-key-properties', [])
+        self.assertEqual(len(key_properties), 0)
 
-        # Check primary key is "UUID" for table that has "events"
-        # Check no key for table that has "merge"
         # Check metadata.
 
 
 
-test1 = TestTable()
+test1 = TestEventsTable()
 test1.setup()
 test1.test_catalog()
+test2 = TestMergeTable()
+test2.setup()
+test2.test_catalog()
+
+
 
 
