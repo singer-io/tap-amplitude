313a670725601ffa64385593705953dc42cbb5da
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-06-12 18:17:13 +0530

    SAC-27713: Finalized discovery and Sync logic

--- tap_amplitude/sync_strategies/incremental.py
+++ tap_amplitude/sync_strategies/incremental.py
@@ -13,7 +13,6 @@ from singer import utils
 
 from tap_amplitude.connection import connect_with_backoff
 
-
 LOGGER = singer.get_logger()
 
 
@@ -26,24 +25,16 @@ def generate_select_sql(catalog_entry, columns):
     select_sql = """
                 SELECT {}
                     FROM {}
-                """.format(','.join(columns), catalog_entry.tap_stream_id.replace('-','.'))
+                """.format(','.join(columns), catalog_entry.tap_stream_id.replace('-', '.'))
 
     return select_sql
 
 
 def process_row(row, columns):
-    utc = pytz.timezone('UTC')
-    row_as_list = list(row)
-
-    for i in range(len(row_as_list)):
-        if isinstance(row_as_list[i], datetime.datetime):
-            row_as_list[i] = utils.strftime(utc.localize(row_as_list[i]))
-
-    return dict(zip(columns, row_as_list))
+    return dict(zip(columns, list(row)))
 
 
 def sync_table(connection, catalog_entry, state, columns):
-    # If there is an existing bookmark, use it; otherwise, use replication_key
     replication_key_value = None
     if not state.get('bookmarks', {}).get(catalog_entry.tap_stream_id):
         singer.write_bookmark(state,
@@ -51,31 +42,24 @@ def sync_table(connection, catalog_entry, state, columns):
                               catalog_entry.replication_key,
                               None)
     else:
-        # Start with the bookmark.
         replication_key_value = singer.get_bookmark(state,
                                                     catalog_entry.tap_stream_id,
                                                     catalog_entry.replication_key)
 
-    # with connection.cursor() as cursor:
     cursor = connection.cursor()
-
-    # Build the sql for this stream.
     select_sql = generate_select_sql(catalog_entry, columns)
 
-    # If bookmark exists, modify the query.
     if replication_key_value is not None:
         if catalog_entry.schema.properties[catalog_entry.replication_key].format == 'date-time':
             replication_key_value = pendulum.parse(replication_key_value)
 
         select_sql += " WHERE {} >= '{}' ORDER BY {} ASC".format(
-                              catalog_entry.replication_key,
-                              replication_key_value,
-                              catalog_entry.replication_key)
-
+            catalog_entry.replication_key,
+            replication_key_value,
+            catalog_entry.replication_key)
     elif catalog_entry.replication_key is not None:
         select_sql += ' ORDER BY {} ASC'.format(catalog_entry.replication_key)
 
-    # time to sync.
     LOGGER.info('Running %s', select_sql)
     cursor.execute(select_sql)
 
@@ -88,17 +72,15 @@ def sync_table(connection, catalog_entry, state, columns):
             counter.increment()
             rows_saved += 1
 
-            # format record
             rec = process_row(row, columns)
 
-            # resolve against metadata
-            with Transformer() as transformer:
-                rec = transformer.transform(rec, catalog_entry.schema.to_dict(), metadata.to_map(catalog_entry.metadata))
+            # Convert datetime.date and datetime.datetime to ISO strings
+            for k, v in rec.items():
+                if isinstance(v, (datetime.datetime, datetime.date)):
+                    rec[k] = v.isoformat()
 
-            # write to singer.
-            singer.write_record(catalog_entry.tap_stream_id, rec)
+            singer.write_record(catalog_entry.stream, rec)  # ✅ This emits the record to output
 
-            # Perhaps the more modern way of managing state.
             if catalog_entry.replication_method == "INCREMENTAL":
                 singer.write_bookmark(state,
                                       catalog_entry.tap_stream_id,
@@ -112,4 +94,4 @@ def sync_table(connection, catalog_entry, state, columns):
 
         singer.write_state(state)
 
-    return counter.value
+        return rows_saved
