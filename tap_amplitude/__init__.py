035ac93ce5e7b9b48bf0b84bc45161bed67a5309
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-06-19 14:08:32 +0530

    Fix: 3 for proper readability and visuality

--- tap_amplitude/__init__.py
+++ tap_amplitude/__init__.py
@@ -33,10 +33,10 @@ Column = collections.namedtuple('Column', [
 ])
 
 REQUIRED_CONFIG_KEYS = [
-    'account', 
-    'warehouse', 
-    'database', 
-    'username', 
+    'account',
+    'warehouse',
+    'database',
+    'username',
     'password'
 ]
 
