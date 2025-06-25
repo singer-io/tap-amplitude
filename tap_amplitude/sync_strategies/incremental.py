059d9cad56432de51c5426a182ef9d3c1db8a273
Romilqlik <romil.totade@qlik.com>
2025-06-25 18:14:50 +0530

    Update incremental.py
    Revert to the existing comments

--- tap_amplitude/sync_strategies/incremental.py
+++ tap_amplitude/sync_strategies/incremental.py
@@ -44,14 +44,18 @@ def sync_table(connection, catalog_entry, state, columns):
                               catalog_entry.replication_key,
                               None)
     else:
+        # start with the bookmark.
         replication_key_value = singer.get_bookmark(state,
                                                     catalog_entry.tap_stream_id,
                                                     catalog_entry.replication_key)
 
+    #with connection.cursor() as cursor:
     cursor = connection.cursor()
+
+    #Build the sql for this schema
     select_sql = generate_select_sql(catalog_entry, columns)
 
-    # Apply replication key filtering
+    # If bookmark exists, modify the query.
     if replication_key_value is not None:
         if catalog_entry.schema.properties[catalog_entry.replication_key].format == 'date-time':
             replication_key_value = pendulum.parse(replication_key_value)
@@ -63,6 +67,7 @@ def sync_table(connection, catalog_entry, state, columns):
     elif catalog_entry.replication_key is not None:
         select_sql += ' ORDER BY {} ASC'.format(catalog_entry.replication_key)
 
+    #time to sync.
     LOGGER.info('Running %s', select_sql)
     cursor.execute(select_sql)
 
@@ -76,6 +81,7 @@ def sync_table(connection, catalog_entry, state, columns):
             counter.increment()
             rows_saved += 1
 
+            #format record
             rec = process_row(row, columns)
 
             # Improved: datetime.date and datetime.datetime as ISO strings
@@ -83,13 +89,14 @@ def sync_table(connection, catalog_entry, state, columns):
                 if isinstance(v, (datetime.datetime, datetime.date)):
                     rec[k] = v.isoformat()
 
-            # Retain metadata transformation logic
+            # resolve against metadata
             with Transformer() as transformer:
                 rec = transformer.transform(rec, catalog_entry.schema.to_dict(), metadata.to_map(catalog_entry.metadata))
 
-            # Use tap_stream_id to stay consistent with bookmark + schema
+            # write to singer.
             singer.write_record(catalog_entry.tap_stream_id, rec)
 
+            # Perhaps the more modern way of managing state.
             if catalog_entry.replication_method == "INCREMENTAL":
                 singer.write_bookmark(state,
                                       catalog_entry.tap_stream_id,
