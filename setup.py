e0646c1c4b273fd9b6fc2e6891d3b48d695b2c60
Scott Nakano <snakano@talend.com>
2026-04-09 18:36:23 +0000

    bump snowflake-connector-python importing pyOpenSSL

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-amplitude',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_amplitude'],
       install_requires=[
-          'snowflake-connector-python==3.15.0',
+          'snowflake-connector-python==3.18.0',
           'attrs==25.3.0',
           'pendulum==3.1.0',
           'pytz==2025.2',
